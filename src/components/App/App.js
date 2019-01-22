80c344284e295a003bfb82be0800ae46edcc51fd
Justin <justin@baal-software.com>
2019-01-22 16:50:00 -0700

    Fetch films from api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      films: []
+    }
+  }
+
+  async getFilms() {
+    const response = await fetch('https://swapi.co/api/films/');
+    const films = await response.json().then(data => data.results);
+    this.setState({ films });
+  }
+
+  componentDidMount() {
+    this.getFilms();
+
+  }
   render() {
     return (
       <div className="App">
-        s
+
       </div>
     );
   }
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  let wrapper = shallow(<App />);
+describe('App', () => {
+  let wrapper;
 
-  expect(wrapper).toMatchSnapshot();
-});
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should fetch films from api', async () => {
+    expect(wrapper.state('films').length).toEqual(0)
+    await wrapper.instance().getFilms();
+    expect(wrapper.state('films').length).not.toEqual(0)
+  });
+
+  it('should get random film scroll text,year, and title', () => {
+    
+  })
+
+
+})
+
