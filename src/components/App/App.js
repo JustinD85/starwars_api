ee43ea5110fd47fc3e909ee555e873f586951a48
Justin <justin@baal-software.com>
2019-01-25 13:41:15 -0700

    Make CardArea Stateful

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,10 +8,6 @@ class App extends Component {
     super();
     this.state = {
       film: {},
-      people: [],
-      vehicles: [],
-      planets: [],
-      species: [],
       currentFilter: null,
     }
   }
@@ -29,74 +25,25 @@ class App extends Component {
     this.setState({ film: { ...films[index] } });
   }
 
-
-
-  getCardAreaData = async (type) => {
-    const data = await this.getData(type)
-    this.setState({
-      [type]: { ...data },
-      currentFilter: type
-    })
-  }
-
-  setAllOfType = async (type, options = '') => {
-    const newData = await this.getData(`${type}${options}`);
-    const prevState = this.state[type];
-    const newState = newData.results;
-    await this.setState({ [type]: [...newState, ...prevState ] })
-    if (newData.next) {
-      const index = newData.next.search(/\?/)
-      const option = newData.next.substring(index);
-      this.setAllOfType(type, option)
-    }
-  }
-
-  setAllData = async () => {
-    const shouldSetSpecies = !this.state.species.length;
-    const shouldSetPlanets = !this.state.planets.length;
-    const shouldSetVehicles = !this.state.vehicles.length;
-    const shouldSetPeople = !this.state.people.length;
-    shouldSetSpecies && this.setAllOfType('species');//4 fetches instead of 10 per page
-    shouldSetPlanets && this.setAllOfType('planets');
-    shouldSetVehicles && this.setAllOfType('vehicles');
-    shouldSetPeople && this.setAllOfType('people');
-  }
-
   setCurrentFilter = (type) => {
     this.setState({
       currentFilter: type
     })
   }
 
-  getNewPageData = async () => {
-    const { currentFilter: type } = this.state;
-    const data = await this.getData(type)
-    // if (unfilteredResult.next) {
-    //   const nextUrl = unfilteredResult.next;
-    //   const index = nextUrl.search(/\?/);
-    //   const options = nextUrl.substring(index);
-    //   console.log(type, options);
-    //   this.getData(type, options);
-    // }
-  }
-
   componentDidMount() {
-    this.setRandomFilm()
-    this.setAllData();
+    this.setRandomFilm();
   }
 
   render() {
-    const { people, vehicles, planets } = this.state;
 
     return (
       <div className="App">
         <ScrollingText film={this.state.film} />
-        <FilterSection getData={this.setAllData} />
+        <FilterSection getData={this.setCurrentFilter} />
         <CardArea
           currentFilter={this.state.currentFilter}
-          people={people.list}
-          vehicles={vehicles.list}
-          planets={planets.list}
+          getData={this.getData}
         />
       </div>
     );
--- src/components/CardArea/CardArea.js
+++ src/components/CardArea/CardArea.js
@@ -1,24 +1,73 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import './CardArea.scss';
 import Card from './Card/Card'
-const CardArea = (props) => {
-  const { currentFilter } = props;
- 
-  return (
-    <section className='CardArea'>
-      {props.currentFilter && props[currentFilter] &&
-        <Fragment>
-          <button className='previous'>{'<'}</button>
-          <button className='next'>{'>'}</button>
+class CardArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: 0,
+      max: 10,
+      people: [],
+      vehicles: [],
+      planets: [],
+      species: [],
+    }
+  }
 
-          {props[currentFilter].map((j) => {
-          
-            return <Card key={j.name}{...j} />
-          })}
-        </Fragment>
-      }
-      {!props.currentFilter && <div>Pick something</div>}
-    </section>)
+  changeNumber = (change) => {
+    this.setState({
+      min: this.state.min + change,
+      max: this.state.max + change,
+    })
+  }
+  setAllOfType = async (type, options = '') => {
+    const { getData } = this.props;
+    const newData = await getData(`${type}${options}`);
+    const prevState = this.state[type];
+    const newState = newData.results;
+    await this.setState({ [type]: [...prevState,...newState] })
+    if (newData.next) {
+      const index = newData.next.search(/\?/)
+      const option = newData.next.substring(index);
+      this.setAllOfType(type, option)
+    }
+  }
+
+  setAllData = async () => {
+    /*
+    Economic to fetch all required data onload asynchronously.
+    Alternative: each card causing at most:
+     - ~3 fetch calls per page
+     - ~10 cards per page
+    Current max cost: ~24calls
+    Alternative cost: ~240+
+    */
+    this.setAllOfType('species');
+    this.setAllOfType('planets');
+    this.setAllOfType('vehicles');
+    this.setAllOfType('people');
+  }
+  componentDidMount() {
+    this.setAllData();
+  }
+  render() {
+    const { currentFilter } = this.props;
+    return (
+      <section className='CardArea'>
+        {this.props.currentFilter &&
+          <Fragment>
+            <button className='previous' onClick={() => this.changeNumber(-10)}>{'<'}</button>
+          { <button className='next' onClick={() => this.changeNumber(10)}>{'>'}</button>}
+
+            {this.state[currentFilter].map((j, i) => {
+              return i > this.state.min && i < this.state.max &&
+                <Card key={j.name}{...j} />
+            })}
+          </Fragment>
+        }
+        {!this.props.currentFilter && <div>Pick something</div>}
+      </section>)
+  }
 }
 
 export default CardArea;
