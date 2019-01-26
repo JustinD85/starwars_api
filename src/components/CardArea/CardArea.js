2421e7c5118667534ed9dc36e75fcc81d763aa0b
Justin <justin@baal-software.com>
2019-01-25 17:07:25 -0700

    Add ability to look at different filter options

--- src/components/CardArea/CardArea.js
+++ src/components/CardArea/CardArea.js
@@ -11,6 +11,7 @@ class CardArea extends Component {
       vehicles: [],
       planets: [],
       species: [],
+      favorites: []
     }
   }
 
@@ -18,19 +19,44 @@ class CardArea extends Component {
     this.setState({
       min: this.state.min + change,
       max: this.state.max + change,
-    })
+    });
   }
+
   setAllOfType = async (type, options = '') => {
     const { getData } = this.props;
     const newData = await getData(`${type}${options}`);
     const prevState = this.state[type];
     const newState = newData.results;
-    await this.setState({ [type]: [...prevState,...newState] })
+    await this.setState({ [type]: [...prevState, ...newState] })
     if (newData.next) {
       const index = newData.next.search(/\?/)
       const option = newData.next.substring(index);
       this.setAllOfType(type, option)
     }
+  }
+
+  getCardInfo = (currentElement) => {
+    const { currentFilter } = this.props;
+    const cardState = {
+      people: () => {
+        const { name, population } = lookUp('planets', 'url', currentElement.homeworld)
+        const { name: speciesName } = lookUp('species', 'url', currentElement.species[0])
+        return [<div>{name}</div>, <div>{population}</div>, <div>{speciesName}</div>]
+      },
+      planets: () => {
+        const { terrain, population, climate } = currentElement;
+        return [<div>{terrain}</div>, <div>{population}</div>, <div>{climate}</div>]
+      },
+      vehicles: () => {
+        const { model, class: _class, passengers } = currentElement;
+        return [<div>{model}</div>, <div>{_class}</div>, <div>{passengers}</div>]
+      },
+    }
+    const lookUp = (noun, aProperty, predicate) => {
+      return this.state[noun].find((element) => element[aProperty] === predicate)
+    }
+
+    return cardState[currentFilter]()
   }
 
   setAllData = async () => {
@@ -39,33 +65,37 @@ class CardArea extends Component {
     Alternative: each card causing at most:
      - ~3 fetch calls per page
      - ~10 cards per page
+     - sluggish app
     Current max cost: ~24calls
     Alternative cost: ~240+
     */
-    this.setAllOfType('species');
     this.setAllOfType('planets');
+    this.setAllOfType('species');
     this.setAllOfType('vehicles');
     this.setAllOfType('people');
   }
+
   componentDidMount() {
     this.setAllData();
   }
+
   render() {
     const { currentFilter } = this.props;
+
     return (
       <section className='CardArea'>
         {this.props.currentFilter &&
           <Fragment>
             <button className='previous' onClick={() => this.changeNumber(-10)}>{'<'}</button>
-          { <button className='next' onClick={() => this.changeNumber(10)}>{'>'}</button>}
+            {<button className='next' onClick={() => this.changeNumber(10)}>{'>'}</button>}
 
-            {this.state[currentFilter].map((j, i) => {
+            {this.state[currentFilter].map((currentElement, i) => {
               return i > this.state.min && i < this.state.max &&
-                <Card key={j.name}{...j} />
+                <Card key={currentElement.name} name={currentElement.name} result={this.getCardInfo(currentElement)} />
             })}
           </Fragment>
         }
-        {!this.props.currentFilter && <div>Pick something</div>}
+        {!this.props.currentFilter && <div>Pick something young Jedi</div>}
       </section>)
   }
 }
