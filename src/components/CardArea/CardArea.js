4e570246ad3baa042346c2d01098de61b4824176
Justin <justin@baal-software.com>
2019-01-26 17:53:19 -0700

    prepare code to utilize localstorage

--- src/components/CardArea/CardArea.js
+++ src/components/CardArea/CardArea.js
@@ -9,24 +9,20 @@ class CardArea extends Component {
       vehicles: [],
       planets: [],
       species: [],
-      favorites: [],
       isLoaded: false,
     }
   }
 
-  changeNumber = (change) => {
-
-    this.setState({
-      min: this.state.min + change,
-      max: this.state.max + change,
-    });
-  }
-
   setAllOfType = async (type, options = '') => {
-    const { getData } = this.props;
+    const { getData, favorites } = this.props;
     const newData = await getData(`${type}${options}`);
     const prevState = this.state[type];
-    const newState = newData.results;
+    const newState = newData.results.map(item => {
+      item.isFavorite = favorites.includes(item.url) ? true : false;
+      item.type = type
+      return item
+    });
+
     await this.setState({ [type]: [...prevState, ...newState] })
     if (newData.next) {
       const index = newData.next.search(/\?/)
@@ -35,10 +31,10 @@ class CardArea extends Component {
     }
   }
 
-  setCard = (currentElement) => {
-    const { currentFilter } = this.props;
+  setCard = (currentElement, type) => {
+    if (!type) type = this.props.currentFilter;
 
-    switch (currentFilter) {
+    switch (type) {
       case 'people': {
         const { name, population } = this.lookUp('planets', 'url', currentElement.homeworld)
         let speciesName;
@@ -57,7 +53,10 @@ class CardArea extends Component {
         const { model, vehicle_class, passengers } = currentElement;
         return [<div>{model}</div>, <div>{vehicle_class}</div>, <div>{passengers}</div>]
       }
-      default: return 'unknown error'
+      case 'favorites': {
+        return 'this.setCard(currentElement, currentElement.type)'
+      }
+      default: throw new Error('Unknown type selected')
     }
   }
 
@@ -66,33 +65,52 @@ class CardArea extends Component {
   }
 
   setAllData = () => {
-    let a = ['planets', 'species', 'vehicles', 'people']
+    let areResolved = ['planets', 'species', 'vehicles', 'people']
       .map(noun => this.setAllOfType(noun))
-    Promise.all(a).then(() => this.setState({ isLoaded: true }))
+    Promise.all(areResolved).then(() => this.setState({ isLoaded: true }))
   }
 
   async componentDidMount() {
     this.setAllData();
   }
 
+  getCard(currentElement) {
+    return (
+      <Card
+        key={currentElement.name}
+        name={currentElement.name}
+        toggleFavorite={this.props.toggleFavorite}
+        id={currentElement.url}
+        result={this.setCard(currentElement)}
+      />)
+  }
+
+  getFavorites = () => {
+    return ['people', 'vehicles', 'planets']
+      .map((items) => [...this.state[items]])
+      .flat()
+      .filter(element => element.isFavorite)
+  }
+
+  getCardsOfType = (filter, min, max) => {
+    return this.state[filter].map((ele, i) => {
+      return i >= min && i <= max && this.getCard(ele)
+    })
+  }
+
+
   render() {
     const { currentFilter, min, max, changeNumber } = this.props;
-
+    const stateOrProp = currentFilter === 'favorites' ? 'props' : 'state';
     return (
       <section className='CardArea'>
         {this.props.currentFilter &&
           <Fragment>
-            {min > 0
-              && <button className='previous' onClick={() => changeNumber(-10)}>{'<'}</button>}
-            {max < this.state[currentFilter].length - 1 &&
+            {min > 0 &&
+              <button className='previous' onClick={() => changeNumber(-10)}>{'<'}</button>}
+            {this.getCardsOfType(currentFilter, min, max)}
+            {max < this[stateOrProp][currentFilter].length - 1 &&
               <button className='next' onClick={() => changeNumber(10)}>{'>'}</button>}
-            {this.state[currentFilter].map((currentElement, i) => {
-              return i > min && i <= max &&
-                <Card
-                  key={currentElement.name}
-                  name={currentElement.name}
-                  result={this.setCard(currentElement)} />
-            })}
           </Fragment>
         }
         {!this.props.currentFilter && this.state.isLoaded && <div>Pick something young Jedi</div>}
