cc2382f35acc2071fef8b180ece955dde43b1c3b
Justin <justin@baal-software.com>
2019-01-26 17:54:05 -0700

    Add dataid to card

--- src/components/CardArea/Card/Card.js
+++ src/components/CardArea/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default (props) => {
-  const { name,result} = props
+  const { name, result, toggleFavorite, id } = props
 
-  return <div className="Card">
+  return <div className="Card" >
+    <div
+      className="favorite-card"
+      onClick={(e) => toggleFavorite(e.target.id)}
+      id={id}
+    >Favorite</div>
     {name}
     {result}
   </div>
