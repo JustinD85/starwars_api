ca9920f7e115658ca591cbbfc501b1b7b9bd3e6c
Justin <justin@baal-software.com>
2019-01-28 08:14:48 -0700

    Updates

---
+++ precache-manifest.8f110c10d73c995e6c403a16c390f647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe998d3b7e35c6055d15",
+    "url": "/starwars_api/static/css/main.e12627b8.chunk.css"
+  },
+  {
+    "revision": "fe998d3b7e35c6055d15",
+    "url": "/starwars_api/static/js/main.fe998d3b.chunk.js"
+  },
+  {
+    "revision": "8a0920ed51edfeef1450",
+    "url": "/starwars_api/static/js/1.8a0920ed.chunk.js"
+  },
+  {
+    "revision": "2e72dc2ddfcb60fc036e",
+    "url": "/starwars_api/static/js/runtime~main.2e72dc2d.js"
+  },
+  {
+    "revision": "5308a7a1714b55eb0599feb4058fa676",
+    "url": "/starwars_api/index.html"
+  }
+];
